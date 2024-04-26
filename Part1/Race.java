a405e065bfa866ced19e70d0eecd2f45ccc05f8a
Raikouben <maharjananish2005@gmail.com>
2024-04-26 17:34:21 +0100

    first commit

---
+++ Part1/Race.java
@@ -0,0 +1,10 @@
+package Part1;
+
+public class Race {
+    private Horse[] horses;
+    private int distance;
+    private int maxHorses;
+    private int numHorses;
+    private int[] positions;
+    private boolean finished
+}
